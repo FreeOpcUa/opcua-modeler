2d226072db679787e2f9e17e1e12042277dcab0c
olivier R-D <olivier@loke>
2016-11-27 11:07:28 +0100

    inline editing/creation of namespaces

--- uamodeler/namespace_widget.py
+++ uamodeler/namespace_widget.py
@@ -1,8 +1,10 @@
-from PyQt5.QtCore import pyqtSignal, Qt, QObject, QModelIndex
+from PyQt5.QtCore import pyqtSignal, Qt, QObject
 from PyQt5.QtGui import QStandardItemModel, QStandardItem
-from PyQt5.QtWidgets import QMenu, QAction, QInputDialog
+from PyQt5.QtWidgets import QMenu, QAction, QInputDialog, QStyledItemDelegate
 
 from opcua import ua
+
+from uawidgets.utils import trycatch
 
 
 class NamespaceWidget(QObject):
@@ -14,6 +16,9 @@ class NamespaceWidget(QObject):
         self.view = view
         self.model = QStandardItemModel()
         self.view.setModel(self.model)
+        delegate = MyDelegate(self.view, self)
+        delegate.error.connect(self.error.emit)
+        self.view.setItemDelegate(delegate)
         self.node = None
         self._namespace_to_delete = None
         self.view.header().setSectionResizeMode(1)
@@ -30,13 +35,20 @@ class NamespaceWidget(QObject):
         self._contextMenu.addAction(self.removeNamespaceAction)
 
     def add_namespace(self):
-        uri, ok = QInputDialog.getText(self.view, "Enter new namespace URI", "URI:")
         uries = self.node.get_value()
-        if uri in uries:
-            return uries.index(uri)
-        uries.append(uri)
-        self.node.set_value(uries)
-        self.reload()
+        newidx = len(uries)
+        it = self.model.item(0, 0)
+        uri_it = QStandardItem("")
+        it.appendRow([QStandardItem(), QStandardItem(str(newidx)), uri_it])
+        idx = self.model.indexFromItem(uri_it)
+        self.view.edit(idx)
+        #uri, ok = QInputDialog.getText(self.view, "Enter new namespace URI", "URI:")
+        #uries = self.node.get_value()
+        #if uri in uries:
+            #return uries.index(uri)
+        #uries.append(uri)
+        #self.node.set_value(uries)
+        #self.reload()
         #it = self.model.item(0, 0)
         #it.appendRow([QStandardItem(), QStandardItem(str(len(uries)+1)), QStandardItem(uri)])
 
@@ -73,8 +85,43 @@ class NamespaceWidget(QObject):
         idx = self.view.currentIndex()
         if not idx.isValid():
             return
-        if idx.parent().isValid() and idx.row() > 1:
+        if idx.parent().isValid() and idx.row() >= 1:
             uri_it = self.model.itemFromIndex(idx.sibling(idx.row(), 2))
             self._namespace_to_delete = uri_it.text()
             self.removeNamespaceAction.setEnabled(True)
         self._contextMenu.exec_(self.view.viewport().mapToGlobal(position))
+
+
+class MyDelegate(QStyledItemDelegate):
+
+    error = pyqtSignal(Exception)
+    attr_written = pyqtSignal(ua.AttributeIds, ua.DataValue)
+
+    def __init__(self, parent, widget):
+        QStyledItemDelegate.__init__(self, parent)
+        self.widget = widget
+
+    @trycatch
+    def createEditor(self, parent, option, idx):
+        print("EDITOR", idx.column())
+        if idx.column() != 2 or idx.row() == 0:
+            return None
+        return QStyledItemDelegate.createEditor(self, parent, option, idx)
+
+    @trycatch
+    def setModelData(self, editor, model, idx):
+        print("SET DATA", editor.text())
+        QStyledItemDelegate.setModelData(self, editor, model, idx)
+        root = model.item(0, 0)
+        uries = []
+        count = 0
+        while True:
+            child = root.child(count, 2)
+            count += 1
+            if not child:
+                break
+            uries.append(child.text())
+        print("Writting namespace array: ", uries)
+        self.widget.node.set_value(uries)
+
+
