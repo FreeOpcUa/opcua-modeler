84d5ba080665fd23d35defb22e435a9cd8cfd4ba
oroulet <olivier@r-dt.net>
2022-02-13 10:47:24 +0100

    new release

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 
 setup(name="opcua-modeler",
-      version="0.5.12",
+      version="0.5.13",
       description="OPC-UA Address Space Modeler",
       author="Olivier R-D et al.",
       url='https://github.com/FreeOpcUa/opcua-modeler',
       packages=["uamodeler"],
       license="GNU General Public License",
-      install_requires=["asyncua>=0.9.9", "opcua-widgets < 0.6.0"],
+      install_requires=["python-opcua>=0.98.13", "opcua-widgets < 0.6.0"],
       entry_points={'console_scripts':
                     ['opcua-modeler = uamodeler.uamodeler:main']
                     }
